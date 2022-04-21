bebbf4189be94179b1d0bd25840aa93af8b16b0b
장병문 <jundead7981@gmail.com>
2022-04-22 00:19:12 +0900

    장병문 : 테스트용 방 개수 되돌림

--- Portfolio/GameEngineContents/RandomRoomManager.cpp
+++ Portfolio/GameEngineContents/RandomRoomManager.cpp
@@ -16,7 +16,7 @@ bool RandomRoomManager::ChangeFloor(const int& _Floor)
 	switch (CurrentFloor_)
 	{
 	case 1:
-		CurrentMapCount_ = 20;
+		CurrentMapCount_ = 9;
 		break;
 	case 2:
 		CurrentMapCount_ = 12;
