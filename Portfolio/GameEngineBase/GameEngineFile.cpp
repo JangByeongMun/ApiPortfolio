b790d28142f4a0211eef63801b781aa0f1036bf5
장병문 <jundead7981@gmail.com>
2022-04-28 23:33:49 +0900

    장병문 : 파일 입출력 기능 준비중

--- Portfolio/GameEngineBase/GameEngineFile.cpp
+++ Portfolio/GameEngineBase/GameEngineFile.cpp
@@ -1,25 +1,65 @@
 #include "GameEngineFile.h"
+#include "GameEngineDebug.h"
 
 GameEngineFile::GameEngineFile() 
+	: FilePtr(nullptr)
 {
 }
 
-GameEngineFile::GameEngineFile(const std::string& _Path)
+GameEngineFile::GameEngineFile(const char* _Path)
+	: FilePtr(nullptr)
 {
 	Path_ = _Path;
 }
 
 GameEngineFile::GameEngineFile(std::filesystem::path _Path)
+	: FilePtr(nullptr)
 {
 	Path_ = _Path;
 }
 
 GameEngineFile::GameEngineFile(const GameEngineFile& _Other)
+	: FilePtr(nullptr)
 {
 	Path_ = _Other.Path_;
 }
 
 GameEngineFile::~GameEngineFile() 
 {
+	GameEngineFile::Close();
+}
+
+void GameEngineFile::Open(OpenMode _Mode)
+{
+	// r = read, w = write
+	// 뒤의 문자가 b냐 t냐에 따라서 저장하는 형식이 달라진다
+	std::string OpenMode = "";
+	switch (_Mode)
+	{
+	case OpenMode::Read:
+		OpenMode = "rb";
+		break;
+	case OpenMode::Write:
+		OpenMode = "wb";
+		break;
+	default:
+		break;
+	}
+
+	fopen_s(&FilePtr, Path_.string().c_str(), OpenMode.c_str());
+
+	if (nullptr == FilePtr)
+	{
+		MsgBoxAssert("파일이 정상적으로 열리지 않았습니다.");
+	}
+}
+
+void GameEngineFile::Close()
+{
+	if (nullptr != FilePtr)
+	{
+		fclose(FilePtr);
+		FilePtr = nullptr;
+	}
 }
 
--- Portfolio/GameEngineBase/GameEngineFile.h
+++ Portfolio/GameEngineBase/GameEngineFile.h
@@ -1,26 +1,42 @@
 #pragma once
 #include "GameEnginePath.h"
 
+enum class OpenMode
+{
+	Read,
+	Write,
+};
+
 // Ό³Έν :
 class GameEngineFile : public GameEnginePath
 {
 public:
 	// constrcuter destructer
 	GameEngineFile();
-	GameEngineFile(const std::string& _path);
+	GameEngineFile(const char* _Path);
 	GameEngineFile(std::filesystem::path _path);
 	GameEngineFile(const GameEngineFile& _Other);
 	~GameEngineFile();
 
-	// delete Function
-	//GameEngineFile(const GameEngineFile& _Other) = delete;
-	//GameEngineFile(GameEngineFile&& _Other) noexcept = delete;
-	GameEngineFile& operator=(const GameEngineFile& _Other) = delete;
-	GameEngineFile& operator=(GameEngineFile&& _Other) noexcept = delete;
+	void Open(OpenMode _Mode);
+	inline void ReadOpen()
+	{
+		Open(OpenMode::Read);
+	}
+	inline void WriteOpen()
+	{
+		Open(OpenMode::Write);
+	}
 
+	void Close();
+	inline void Create()
+	{
+		Open(OpenMode::Write);
+		Close();
+	}
 protected:
 
 private:
-
+	FILE* FilePtr;
 };
 
